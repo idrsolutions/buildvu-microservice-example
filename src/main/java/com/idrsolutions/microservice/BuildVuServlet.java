573010e9766f26cd75b0e335b8d9450e14299490
Sam Howard <sam@idrsolutions.com>
2021-01-20 17:33:23 +0000

    Update copyright year

--- src/main/java/com/idrsolutions/microservice/BuildVuServlet.java
+++ src/main/java/com/idrsolutions/microservice/BuildVuServlet.java
@@ -3,7 +3,7 @@
  *
  * Project Info: https://github.com/idrsolutions/buildvu-microservice-example
  *
- * Copyright 2018 IDRsolutions
+ * Copyright 2021 IDRsolutions
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
