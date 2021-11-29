c81e95ecb77a4e015f1e75e720d6866cb46fcad8
Kieran-IDR <kieran@idrsolutions.com>
2021-11-29 10:14:12 +0000

    Update the context listeners to move all stream handling into the base class

--- src/main/java/com/idrsolutions/microservice/BuildVuServletContextListener.java
+++ src/main/java/com/idrsolutions/microservice/BuildVuServletContextListener.java
@@ -1,36 +1,19 @@
 package com.idrsolutions.microservice;
 
-import javax.servlet.ServletContext;
-import javax.servlet.ServletContextEvent;
 import javax.servlet.annotation.WebListener;
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.FileNotFoundException;
-import java.io.InputStream;
 
 @WebListener
 public class BuildVuServletContextListener extends BaseServletContextListener {
 
-    @Override
-    public void contextInitialized(final ServletContextEvent servletContextEvent) {
+    public String getConfigPath() {
         String userDir = System.getProperty("user.home");
         if (!userDir.endsWith("/") && !userDir.endsWith("\\")) {
             userDir += System.getProperty("file.separator");
         }
-
-        final File externalFile = new File(userDir + "/.idr/buildvu-microservice/buildvu-microservice.properties");
-
-        try {
-            if (externalFile.exists()) {
-                propertiesFile = new FileInputStream(externalFile.getAbsolutePath());
-            } else {
-                propertiesFile = servletContextEvent.getServletContext().getResourceAsStream("buildvu-microservice.properties");
-            }
-        } catch (FileNotFoundException e) {
-            e.printStackTrace();
-        }
-
-        super.contextInitialized(servletContextEvent);
+        return userDir + "/.idr/buildvu-microservice/";
     }
 
+    public String getConfigName(){
+        return "buildvu-microservice.properties";
+    }
 }
