0bda1005a9bf5dc1041bd3fa4d0e2bb9a1850251
KieranFrance <kieran@idrsolutions.com>
2021-12-08 14:04:51 +0000

    Add additional settings for discussion

--- src/main/java/com/idrsolutions/microservice/BuildVuServlet.java
+++ src/main/java/com/idrsolutions/microservice/BuildVuServlet.java
@@ -52,12 +52,6 @@ public class BuildVuServlet extends BaseServlet {
 
     private static final Logger LOG = Logger.getLogger(BuildVuServlet.class.getName());
 
-    static {
-        setInputPath(USER_HOME + "/.idr/buildvu-microservice/input/");
-        setOutputPath(USER_HOME + "/.idr/buildvu-microservice/output/");
-        OutputFileServlet.setBasePath(USER_HOME + "/.idr/buildvu-microservice/output");
-    }
-
     private static final String[] validTextModeOptions = {
             "svg_realtext",
             "svg_shapetext_selectable",
