fa39f8f7ec767b17183646f93f5484685ccae4a3
Jacob Lucas <Jacob@idrsolutions.com>
2022-02-02 17:10:51 +0000

    Added finality to variables that can have it

--- src/main/java/com/idrsolutions/microservice/BuildVuServlet.java
+++ src/main/java/com/idrsolutions/microservice/BuildVuServlet.java
@@ -84,7 +84,7 @@ public class BuildVuServlet extends BaseServlet {
 
         final Map<String, String> conversionParams;
         try {
-            Map<String, String> settings = DBHandler.INSTANCE.getSettings(uuid);
+            final Map<String, String> settings = DBHandler.INSTANCE.getSettings(uuid);
             conversionParams = settings != null ? settings : new HashMap<>();
         } catch (final SQLException e) {
             DBHandler.INSTANCE.setError(uuid, 500, "Database failure");
